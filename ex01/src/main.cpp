3a810c9bcaee9d2440c26c2c16453a756ae0d55a
Suhana Sajid <73544349+suhana66@users.noreply.github.com>
2024-09-16 17:29:33 +0400

    handled ctrl+d

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,16 +6,29 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:52:18 by susajid           #+#    #+#             */
-/*   Updated: 2024/09/16 12:58:33 by susajid          ###   ########.fr       */
+/*   Updated: 2024/09/16 17:27:08 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Phonebook.hpp"
+#include <iostream>
+#include <string>
 
 int	main(void)
 {
 	Phonebook	phonebook;
+	std::string	str;
 
-	(void)phonebook;
+	while (str != "EXIT")
+	{
+		std::cout << "Enter a command > ";
+		std::getline(std::cin, str);
+		if (std::cin.eof())
+		{
+			std::cout << std::endl;
+			std::cout << "* Exiting phonebook now... (CTRL+D was pressed)" << std::endl;
+			break ;
+		}
+	}
 	return (0);
 }
