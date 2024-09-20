801ac15a9e075d3e0a33f4adcb3632703f5c0e2a
Suhana Sajid <73544349+suhana66@users.noreply.github.com>
2024-09-20 10:20:49 +0400

    added getters for private static members

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,8 +6,35 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 21:54:47 by susajid           #+#    #+#             */
-/*   Updated: 2024/09/19 22:37:52 by susajid          ###   ########.fr       */
+/*   Updated: 2024/09/20 10:18:01 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Code understanding: https://chatgpt.com/share/66ec6ef4-25f8-8005-a29b-ecf3f99ac081
+
+#include "Account.hpp"
+
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+int	Account::getNbAccounts( void )
+{
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (Account::_totalNbWithdrawals);
+}
